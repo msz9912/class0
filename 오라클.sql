68c2c0a549aa948ef060c8e524e4113fd7c61075
조민서 <alstjsmsrnt@naver.com>
2025-05-13 12:34:31 +0900

    SQL 최초 등록

---
+++ 오라클.sql
@@ -0,0 +1,28 @@
+-- sal 컬럼을 기준으로 오름차순 - 작은 것부터 큰 것 순서로 
+select * from emp
+order by sal;
+
+select * from emp
+order by asc;
+
+-- sal 컬럼 기준으로 내림차순 - 큰 것부터 순서로
+select * from emp
+order by sal desc;
+
+-- 가장 먼저 입사한 순서로 출력
+select * from emp
+order by hiredate asc;
+
+select * from emp
+order by ename desc;
+
+select * from emp
+order by deptno asc, sal desc;
+-- deptno asc, sal desc; 이 상황에서 연봉이 같으면 사원번호 내림차순 정렬
+
+select * from emp
+order by deptno asc, sal desc, empno desc;
+
+
+
+
